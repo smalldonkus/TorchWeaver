68054265ad0d2d0654949808f26a4258a0017f77
Jacobiee <jacobemcgraw@gmail.com>
2025-10-07 14:06:31 +1100

    added import for React Flow and put sample code for it in the Canvas component page.

--- frontend/src/app/canvas/page.tsx
+++ frontend/src/app/canvas/page.tsx
@@ -1,10 +1,48 @@
+"use client"
 import Link from "next/link"
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
+import { useState, useCallback } from 'react';
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
 
 export default function CanvasPage() {
+     const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+    
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+  );
     return (
-        <>
-            <h1>Canvas TODO page</h1>
 
+        <>
+            
+            <h1>Canvas TODO page - React Flow "Get Started" default has been added</h1>
+            <div style={{ width: '80vw', height: '50vh' }}>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    fitView
+                />
+            </div>
             <div className="routes">
                 <Link href="/">
                     <button>Back to landing page</button>
