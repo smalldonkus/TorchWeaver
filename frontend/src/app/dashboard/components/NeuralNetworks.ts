6a7aa528a70a6be8d87a2898fb68053f16bf5c38
Oscar Chen <z5425727@ad.unsw.edu.au>
2025-11-02 23:42:24 +1100

    removed redundancy and testing code

--- frontend/src/app/dashboard/components/NeuralNetworks.ts
+++ frontend/src/app/dashboard/components/NeuralNetworks.ts
@@ -12,12 +12,9 @@ let NeuralNetworks: NeuralNetworkInfo[] = [
     { title: "C Test Neural Network", lastAccessed: "30/30/3000", image: "/testnetwork.png" , Owner:"B", Favourited: true},
 ];
 
-let VisibleNetworks: NeuralNetworkInfo[] = [];
-
 export const getNeuralNetworks = () => NeuralNetworks;
 
 export const setNeuralNetworks = (NewNeuralNetworks: NeuralNetworkInfo[]) => {
-  console.log(NewNeuralNetworks);
   NeuralNetworks = NewNeuralNetworks;
 };
 
