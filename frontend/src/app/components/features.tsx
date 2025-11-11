8362707922bd05192786fb87156047aa809ee74d
Jacobiee <jacobemcgraw@gmail.com>
2025-11-12 00:02:34 +1100

    landing page re-done to look more modern

--- frontend/src/app/components/features.tsx
+++ frontend/src/app/components/features.tsx
@@ -26,6 +26,28 @@ export default function Features() {
                         heading={"Streamlined Access and Design"}
                         notes={"User responsive page design and streamlined access allows you to get creating straight away and with no pay-walls or significant loading times. Create now!"}
                     />
+
+                    <Component 
+                        image={"dnd.gif"}
+                        heading={"Drag-and-Drop Functionality"}
+                        notes={"Easy visual framework that allows a user to construct their desirable neural network architecture with no hard-coding."}
+                    />
+                    <Component 
+                        image={"layers.gif"}
+                        heading={"Pre-designed Selection of Layers and Tensors"}
+                        notes={"Torchweaver comes with a range of pre-built nodes to place in layers and tensor operations of your choosing. Currently supports the full design of AlexNet."}
+                    />
+                    <Component 
+                        image={"export.png"}
+                        heading={"Export Architecture Capabilities"}
+                        notes={"When an architecture is visually completed with parameters filled, the code for the framework can be exported and generated with ready-to-go Pytorch, allowing quick use for model training."}
+                    />
+
+                    <Component 
+                        image={"background-img.jpg"}
+                        heading={"Streamlined Access and Design"}
+                        notes={"User responsive page design and streamlined access allows you to get creating straight away and with no pay-walls or significant loading times. Create now!"}
+                    />
                 </div>
             </div>
 
